9eb31914c7882e3cd94c465a6a83c91c8a5e075c
GeniusK <geniusk1047@naver.com>
2018-10-02 00:22:15 +0900

    billboard 작동은 되는데

--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -49,34 +49,36 @@ export default {
     }
   },
   async mounted () {
+    const chart = bb.generate({
+      data: {
+        x: 'x',
+        columns: [
+          ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
+          ['data1', 30, 200, 100, 400, 150, 250],
+          ['data2', 130, 340, 200, 500, 250, 350]
+        ]
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%Y-%m-%d'
+          }
+        }
+      },
+      bindto: '#TimeseriesChart'
+    })
+
+    setTimeout(function() {
+      chart.load({
+        columns: [
+          ['data3', 400, 500, 450, 700, 600, 500]
+        ]
+      })
+    }, 1000)
     const { data } = await axios.get('http://localhost:5000/api/customers')
     this.datas = data
   }
 }
 
-var chart = bb.generate({
-  data: {
-    x: 'x',
-    columns: [
-      ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06'],
-      ['data1', 30, 200, 100, 400, 150, 250],
-      ['data2', 130, 340, 200, 500, 250, 350]
-    ]
-  },
-  axis: {
-    x: {
-      type: 'timeseries',
-      tick: {
-        format: '%Y-%m-%d'
-      }
-    }
-  },
-  bindto: '#TimeseriesChart'
-})
-
-chart.load({
-    columns: [
-    g'data3', 400, 500, 450, 700, 600, 500]
-    ]
-})
 </script>
